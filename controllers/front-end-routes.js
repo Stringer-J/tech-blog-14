04bed1bdd4876f296af416f78950428c5ea4547c
Josh Stringer <joshstringer@live.com>
2024-08-07 14:41:18 -0500

    work on login page, added basic form

--- controllers/front-end-routes.js
+++ controllers/front-end-routes.js
@@ -1,14 +1,5 @@
 const router = require('express').Router();
 
-// router.get('/dashboard', async (req, res) => {
-//     try {
-//         res.render('dashboard');
-//     } catch (error) {
-//         console.error('Error rendering dashboard:', error);
-//         res.status(500).send('Internal Server Error');
-//     }
-// });
-
 router.get('/signup', async (req, res) => {
     try {
         res.render('signup');
