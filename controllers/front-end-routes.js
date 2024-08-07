0c404b52845eda919e11e751d648b85aaaacc1e7
Josh Stringer <joshstringer@live.com>
2024-08-07 12:29:09 -0500

    added a few new front end routes, added a few more test blogs

--- controllers/front-end-routes.js
+++ controllers/front-end-routes.js
@@ -9,4 +9,22 @@ router.get('/dashboard', async (req, res) => {
     }
 });
 
+router.get('/signup', async (req, res) => {
+    try {
+        res.render('signup');
+    } catch (error) {
+        console.error('Error rendering signup:', error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+router.get('/login', async (req, res) => {
+    try {
+        res.render('login');
+    } catch (error) {
+        console.error('Error rendering login:', error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
 module.exports = router;
