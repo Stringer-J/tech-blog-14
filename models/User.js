fac2845e6201aa78bd797c77cbbccfe5545100de
Josh Stringer <joshstringer@live.com>
2024-08-08 09:24:32 -0500

    added a tags to login/signup, created User model

---
+++ models/User.js
@@ -0,0 +1,30 @@
+const { Model, Datatypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class User extends Model {}
+
+User.init(
+    {
+        id: {
+            type: Datatypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        user_name: {
+            type: Datatypes.STRING,
+            allowNull: false,
+        },
+        password: {
+            type: Datatypes.STRING,
+            allowNull: false,
+        },
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        modelName: 'user',
+    }
+);
+
+module.exports = User;
