38a2b3e9942b3b3a2259e3e446757a6a7af0bc18
Josh Stringer <joshstringer@live.com>
2024-08-10 13:40:49 -0500

    added dark overlay under form

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,11 +1,16 @@
-const newButton = document.getElementById('newBlog');
-const formBox = document.getElementById('formBox');
+document.addEventListener('DOMContentLoaded', () => {
+    const newButton = document.getElementById('newBlog');
+    const formBox = document.getElementById('formBox');
+    const overlay = document.getElementById('overlay');
 
-newButton.addEventListener('click', () => {
-    // alert('Click Worked');
-    if (formBox.classList.contains('hidden')) {
-        formBox.classList.remove('hidden');
-    } else {
+    newButton.addEventListener('click', () => {
+        const isHidden = formBox.classList.contains('hidden');
+        formBox.classList.toggle('hidden', !isHidden);
+        overlay.classList.toggle('hidden', !isHidden);
+    });
+
+    overlay.addEventListener('click', () => {
         formBox.classList.add('hidden');
-    }
+        overlay.classList.add('hidden');
+    });
 });
