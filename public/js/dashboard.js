1c20cc0340d55c84e716e0963d72bcfc7c26cff1
Josh Stringer <joshstringer@live.com>
2024-08-10 13:23:54 -0500

    when you click the new button on dashboard, a form for making a new blog appears

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,2 +1,11 @@
 const newButton = document.getElementById('newBlog');
+const formBox = document.getElementById('formBox');
 
+newButton.addEventListener('click', () => {
+    // alert('Click Worked');
+    if (formBox.classList.contains('hidden')) {
+        formBox.classList.remove('hidden');
+    } else {
+        formBox.classList.add('hidden');
+    }
+});
