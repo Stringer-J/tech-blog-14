780b9f7e41bce7faae678352e87961872225263d
Josh Stringer <joshstringer@live.com>
2024-08-10 14:26:29 -0500

    added functionality to submit button when adding new blog post, just have to create route to get it going

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -2,6 +2,9 @@ document.addEventListener('DOMContentLoaded', () => {
     const newButton = document.getElementById('newBlog');
     const formBox = document.getElementById('formBox');
     const overlay = document.getElementById('overlay');
+    const titleInput = document.getElementById('title');
+    const contentInput = document.getElementById('content');
+    const newSubmit = document.getElementById('newSubmit');
 
     newButton.addEventListener('click', () => {
         const isHidden = formBox.classList.contains('hidden');
@@ -13,4 +16,40 @@ document.addEventListener('DOMContentLoaded', () => {
         formBox.classList.add('hidden');
         overlay.classList.add('hidden');
     });
+
+    newSubmit.addEventListener('click', () => {
+        const tValue = titleInput.value;
+        const cValue = contentInput.value;
+        const user_name = sessionStorage.getItem('user_name');
+
+        if (tValue.trim() !== "" && cValue.trim() !== "") {
+            fetch('/api/blog', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    title: tValue,
+                    posted: user_name,
+                    content: cValue
+                }),
+            })
+            .then(response => response.json())
+            .then(data => {
+                if (data.message === 'Blog created') {
+                    console.log('Success', data);
+                    alert('Blog Created');
+                    setTimeout(() => {
+
+                    }, 100);
+                } else {
+                    console.error('Blog Creation failed:', data.message);
+                }
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            })
+        }
+    })
+
 });
