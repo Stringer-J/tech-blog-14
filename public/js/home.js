92d9050916fad980262b7634b04f7fd6e19ac858
Josh Stringer <joshstringer@live.com>
2024-08-14 09:02:48 -0500

    when you click the comment button, a form appears allowing you to write your comment

--- public/js/home.js
+++ public/js/home.js
@@ -1,15 +1,20 @@
 const commentButton = document.querySelectorAll('.commentButton');
-
-// function isLoggedInForComment (req, res, next) {
-//     if (req.session.user) {
-//         return next();
-//     }
-//     console.log('Comments Allowed');
-// }
+const formBox3 = document.getElementById('formBox3');
+const overlay3 = document.getElementById('overlay3');
+const commentInput = document.getElementById('comment');
+const comSubmit = document.getElementById('comSubmit');
 
 commentButton.forEach(button => {
     button.addEventListener('click', function() {
         // isLoggedInForComment();
-        alert('button clicked my friend');
+        // alert('button clicked my friend');
+        const isHidden = formBox3.classList.contains('hidden');
+        formBox3.classList.toggle('hidden', !isHidden);
+        overlay3.classList.toggle('hidden', !isHidden);
     });
+});
+
+overlay3.addEventListener('click', () => {
+    formBox3.classList.add('hidden');
+    overlay3.classList.add('hidden');
 });
