fde2cab19be6f24b2340a2ab93234e00d441d147
Josh Stringer <joshstringer@live.com>
2024-08-16 16:48:55 -0500

    MORE render work

--- models/index.js
+++ models/index.js
@@ -1,3 +1,4 @@
+(async () => {
 const User = require('./User');
 const Blog = require('./Blog');
 const Comment = require('./Comment');
@@ -11,3 +12,4 @@ Blog.hasMany(Comment, { foreignKey: 'blog_id'});
 Comment.belongsTo(Blog, { foreignKey: 'blog_id'});
 
 module.exports = { User, Blog, Comment };
+})();
